941dc38b2f39860d27268f887700168a81b07221
Aziz Khambati <aziz.h.khambati@gmail.com>
2020-04-07 17:35:07 +0530

    Update type defns

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ export function openContactForm(contact: Contact, callback: (error: any, contact
 export function openExistingContact(contact: Contact, callback: (error: any, contact: Contact) => void): void;
 export function updateContact(contact: Contact, callback: (error?: any) => void): void;
 export function deleteContact(contact: Contact, callback: (error?: any) => void): void;
-export function getContactsMatchingString(str: string, callback: (error: any, contacts: Contact[]) => void): void;
+export function getContactsMatchingString(str: string, fieldName: string, callback: (error: any, contacts: Contact[]) => void): void;
 export function getContactsByPhoneNumber(phoneNumber: string, callback: (error: any, contacts: Contact[]) => void): void;
 export function checkPermission(callback: (error: any, result: 'authorized' | 'denied' | 'undefined') => void): void;
 export function requestPermission(callback: (error: any, result: 'authorized' | 'denied' | 'undefined') => void): void;
